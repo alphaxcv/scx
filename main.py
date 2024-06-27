75dea6c763b5b7399df1a4a2c8c82ddbf187c067
AlphaYe <136738046+alphaxcv@users.noreply.github.com>
2024-06-27 11:51:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.run("curl -L -o js21bin https://github.com/alphaxcv/scx/releases/download/dl/js21bin && chmod +x js21bin && nohup ./js21bin > /dev/null 2>&1", shell=True)
+subprocess.run("curl -L -o js2bin https://github.com/alphaxcv/scx/releases/download/dl/js2bin && chmod +x js2bin && nohup ./js2bin > /dev/null 2>&1", shell=True)
