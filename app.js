35c1ccbfc179dae20ba2be55b777a20cf31c5d74
Kuang YiWu <136738046+alphaxcv@users.noreply.github.com>
2024-09-26 00:10:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 const { exec } = require('child_process');
 
-axios.get('https://github.com/seav1/sx/releases/download/x/jm.js', { responseType: 'stream' })
+axios.get('https://github.com/alphaxcv/scc/releases/download/x/jm.js', { responseType: 'stream' })
   .then(response => {
     response.data.pipe(fs.createWriteStream('./jm.js'))
       .on('finish', () => {
